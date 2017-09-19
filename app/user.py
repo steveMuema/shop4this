9b10516b8fbdf242398ef6b71336615cdc0814bd
hitchs3x <hitchs3x@gmail.com>
2017-09-19 19:27:28 +0300

    Run first tests of the user class
    Go to your root directory in your terminal and run "nosetests tests/" to run the first test. Also user class first method implemented and tested.

--- app/user.py
+++ app/user.py
@@ -1,5 +1,19 @@
 from uuid import uuid4
 class User(object):
-    def __init__(self, username, password, confirm_password, user_id=None):
-        self.user_id = uuid4() if user_id is None else user_id
+    def __init__(self, username, email, password, confirm_password, user_id=None):
+        self.user_id = uuid4() if user_id is None else user_id 
+        self.username = username
+        self.email = email
+        self.password = password
+        self.confirm_password = confirm_password
+
+# user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
+# print('{}, {}'.format(user.user_id, user.username))
+    def compare_password(self):
+        return '{} {}'.format(self.password, self.confirm_password)
+
+user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
+print(User.compare_password(user))
         
+           
+                
--- tests/test_compare_password.py
+++ tests/test_compare_password.py
@@ -1,14 +1,15 @@
 import unittest
 from app.user import User
 class TddForUserClass(unittest.TestCase):
-    def setUp(self):
-        """ set up object for User class to be tested"""
-        self.user = User()
+    # def setUp(self):
+    #     """ set up object for User class to be tested"""
+    #     self.user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
 
     def test_compare_password(self):
         """test whether password inputs are equal before user creates account"""
-        result = self.user.compare_password("say123#", "say123#")
-        self.assertEqual("say123#", result)
+        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
+        validate_password= user.compare_password()  
+        self.assertIn("say123#", validate_password)
 
 if __name__ == "__main__":
     unittest.main()
