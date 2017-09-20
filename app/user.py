1e9e5f37f077e87dd17a6d164773b3da521ff529
hitchs3x <hitchs3x@gmail.com>
2017-09-20 08:56:38 +0300

    Refractor user model class
    tests and implementation of methods to user class. user can create an account and log in to account

--- app/user.py
+++ app/user.py
@@ -1,7 +1,8 @@
 from uuid import uuid4
 class User(object):
+    user_list=[]
     def __init__(self, username, email, password, confirm_password, user_id=None):
-        self.user_id = uuid4() if user_id is None else user_id 
+        self.user_id = str(uuid4()) if user_id is None else user_id 
         self.username = username
         self.email = email
         self.password = password
@@ -10,10 +11,42 @@ class User(object):
 # user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
 # print('{}, {}'.format(user.user_id, user.username))
     def compare_password(self):
-        return '{} {}'.format(self.password, self.confirm_password)
+        if self.password is self.confirm_password:
+            return '{}'.format(self.password)
+        else:
+            return "The passwords do not match."
 
-user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
-print(User.compare_password(user))
-        
-           
-                
+    def registration_store(self):
+        """ method to register user credentials in a list"""
+        user_validation = self.email_exists()
+        if user_validation is False:
+            # return "Account exists. Sign in to the account", self.email 
+            user_credentials = {'user_id' : self.user_id,
+                                'username': self.username,
+                                'email': self.email,
+                                'password': self.password,}
+            self.user_list.append(user_credentials)
+            return user_credentials   
+        else:
+            return user_validation
+    
+    def email_exists(self):
+        """ validates whether there is an existing email before creating a new account """
+        all_mails = [exists['email'] for exists in User.user_list if self.email == exists['email']]
+        if self.email in all_mails:
+            return "Account exists. Sign in to the account"+', '+ self.email            
+        else:
+            return False
+
+    def signin(self, email, password):
+        auth_email = [account_email['email'] for account_email in User.user_list if self.email is account_email['email']]
+       
+        auth_pswd = [ account_pswd['password'] for account_pswd in User.user_list if self.password is account_pswd['password'] ]
+        auth_account=(auth_email, auth_pswd)
+        auth_input = ([email], [password])
+        if auth_account != auth_input:
+            return "Log in unsuccessful, please retry."
+        else:
+            return "Successfully signed in"
+       
+    
--- tests/test_user.py
+++ tests/test_user.py
@@ -9,7 +9,16 @@ class TddForUserClass(unittest.TestCase):
         """test whether password inputs are equal before user creates account"""
         user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
         validate_password= user.compare_password()  
-        self.assertIn("say123#", validate_password)
+        self.assertEqual("say123#", validate_password)
+    
+    def test_registration_store(self):
+        """ tests whether the user dictionary is saved in user_list"""
+        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
+        self.assertListEqual([user.registration_store()], User.user_list)
+    def test_signin(self):
+        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")        
+        User.registration_store(user)
+        user_login = self.signin("stevemuema27@gmail.com", 'say123#')
 
 if __name__ == "__main__":
     unittest.main()
