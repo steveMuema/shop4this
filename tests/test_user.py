4a896a968559308982b4607a8ab495507b4d9624
hitchs3x <hitchs3x@gmail.com>
2017-09-20 11:24:59 +0300

    Hotfix user model tests
    Code refractored according to PEP8 standards

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,24 +1,23 @@
 import unittest
 from app.user import User
 class TddForUserClass(unittest.TestCase):
-    # def setUp(self):
-    #     """ set up object for User class to be tested"""
-    #     self.user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
-
+    """ Tests all implemented methods for accessing user class """
+    def setUp(self):
+        """ set up object for User class to be tested"""
+        self.user = User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
     def test_compare_password(self):
         """test whether password inputs are equal before user creates account"""
-        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
-        validate_password= user.compare_password()  
-        self.assertEqual("say123#", validate_password)
-    
+        validate_password = self.user.compare_password()  
+        self.assertEqual("say123#", validate_password)   
     def test_registration_store(self):
         """ tests whether the user dictionary is saved in user_list"""
-        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")
-        self.assertListEqual([user.registration_store()], User.user_list)
-    def test_signin(self):
-        user= User('Steve Muema', 'stevemuema@gmail.com','say123#', "say123#")        
-        User.registration_store(user)
-        user_login = self.signin("stevemuema27@gmail.com", 'say123#')
+        add_user = self.user.registration_store()
+        self.assertEqual([add_user], User.user_list)
+    def test_successful_signin(self):
+        """ tests whether the sign in attempt was successful"""
+        login_user = self.user.registration_store()
+        check_user = self.user.signin('stevemuema@gmail.com', 'say123#')
+        self.assertEquals("Successfully signed in", check_user)
 
 if __name__ == "__main__":
     unittest.main()
